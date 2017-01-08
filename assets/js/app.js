e29e32542e1fedc25a59305cc8198ecd6c52c285
YRadov <radov.yuriy@ukr.net>
2017-01-08 11:13:49 +0200

    add comments

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,6 +16,12 @@ var home_app = new Vue({
             ru: Words[0].ru,
             en: Words[0].en,
             tr: Words[0].tr
+        },
+        page: {
+            home: true,
+            search: false,
+            add: false,
+            info: false
         }
     },
     computed: {
@@ -39,6 +45,7 @@ var home_app = new Vue({
             this.lang.en = true;
             this.lang.ru = true;
         },
+        // переход к предыдущему слову
         prevWord: function () {
             this.wordCnt--;
             if (this.wordCnt < 0) {
@@ -46,6 +53,7 @@ var home_app = new Vue({
             }
             this.changeWord();
         },
+        // переход к следующему слову
         nextWord: function () {
             this.wordCnt++;
             if (this.wordCnt >= WordsLength) {
@@ -53,16 +61,27 @@ var home_app = new Vue({
             }
             this.changeWord();
         },
+        // переход в начало списка
         startWord: function () {
             this.wordCnt = 0;
             this.changeWord();
         },
+        // изменение одного слова
         changeWord: function() {
             this.word.ru =  this.internalWords[this.wordCnt].ru;
             this.word.en =  this.internalWords[this.wordCnt].en;
             this.word.tr =  this.internalWords[this.wordCnt].tr;
         },
+        // смена категории
         changeCat: function (event) {
+            this.startWord();
+        },
+        // скрыть все страницы
+        pagesHide: function  () {
+
+        },
+        // переключение страниц
+        pageHome: function (event) {
             this.startWord();
         }
     }
